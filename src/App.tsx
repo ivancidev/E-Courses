9a9c35c91c4bb68792b5d9d215acf93c7095b20e
Herlan <herlanherbas@gmail.com>
2024-08-25 16:30:54 -0400

    chore: Add MirageJS server configuration for development environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { router } from './routes/router';
+import mackServer from './mirage/server';
+
+if(process.env.NODE_ENV === 'development') {
+  mackServer({ environment: 'development' });
+}
 
 function App() {
   return <RouterProvider router={router} />;
