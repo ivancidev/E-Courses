e9a5556b2fafd76eb0892f89596e212ac6882cf3
Herlan <herlanherbas@gmail.com>
2024-08-25 16:30:30 -0400

    feat: Fetch and display courses from API in Courses component

--- src/pages/courses/courses.tsx
+++ src/pages/courses/courses.tsx
@@ -1,3 +1,45 @@
+import { useEffect, useState } from 'react';
+
+interface Course {
+  id: string;
+  title: string;
+  description: string;
+  duration: string;
+}
+
 export default function Courses() {
-  return <div>courses</div>;
+  const [courses, setCourses] = useState<Course[]>([]);
+  const [isloading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        const response = await fetch('/api/courses');
+        const data = await response.json();
+        setCourses(data.courses);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(true);
+        console.log(error);
+      }
+    };
+    fetchCourses();
+  }, []);
+
+  return (
+    <div>
+      <h1>Courses</h1>
+      {isloading ? (
+        <p>Loading!!</p>
+      ) : (
+        courses.map((course) => (
+          <div key={course.id}>
+            <h2>{course.title}</h2>
+            <p>{course.description}</p>
+            <p>{course.duration}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
 }
