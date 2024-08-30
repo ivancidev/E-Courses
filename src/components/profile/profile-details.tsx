6d937c96faee3ba02280237199b21193838e90a0
Herlan <herlanherbas@gmail.com>
2024-08-30 12:54:50 -0400

    feat: Add ProfileDetails component to Profile page

---
+++ src/components/profile/profile-details.tsx
@@ -0,0 +1,11 @@
+import ProfileIcon from '../shared/ui/icons/profile';
+
+export default function ProfileDetails() {
+  return (
+    <div>
+      <div>
+        <ProfileIcon />
+      </div>
+    </div>
+  );
+}
