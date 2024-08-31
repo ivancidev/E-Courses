769940eadcc63e52dbdfb9dfdfc335e6e14bb75c
Herlan <herlanherbas@gmail.com>
2024-08-30 20:04:40 -0400

    refactor: Update Profile page layout and components

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,9 +1,17 @@
-import ProfileDetails from "../../components/profile/profile-details";
+import Achievements from '../../components/profile/achievements';
+import CourseProgress from '../../components/profile/course-progress';
+import ProfileDetails from '../../components/profile/profile-details';
+import Stats from '../../components/profile/stats';
 
 export default function Profile() {
   return (
-    <div>
-      <ProfileDetails />
+    <div className="flex h-full justify-center items-center w-full">
+      <div className="w-full p-8">
+        <ProfileDetails />
+        <Stats />
+        <CourseProgress />
+        <Achievements />
+      </div>
     </div>
-  )
+  );
 }
