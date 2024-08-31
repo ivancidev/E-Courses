31d8d42658c7f2195d1371080b4fe8c58040daf7
Herlan <herlanherbas@gmail.com>
2024-08-30 23:04:08 -0400

    refactor: Update import paths and formatting in root.tsx

--- src/layouts/root/root.tsx
+++ src/layouts/root/root.tsx
@@ -1,5 +1,5 @@
-import { Outlet } from "react-router-dom";
-import Navbar from "../../components/shared/navbar";
+import { Outlet } from 'react-router-dom';
+import Navbar from '../../components/shared/navbar';
 
 export default function Root() {
   return (
