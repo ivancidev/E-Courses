1ee245b0664b6fc3cccb4eebe9bd627bfdecf8d6
Herlan <herlanherbas@gmail.com>
2024-08-30 23:03:34 -0400

    feat: Add sign-in functionality and protected routes

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -3,23 +3,41 @@ import Root from '../layouts/root/root';
 import Hero from '../pages/home/hero';
 import Courses from '../pages/courses/courses';
 import Profile from '../pages/profile/profile';
+import SlotPropsSignIn from '../pages/login/login';
+import ProtectedRoute from './protected-route';
 
 export const router = createBrowserRouter([
   {
     path: '/',
+    element: <SlotPropsSignIn />,
+  },
+  {
+    path: '/app',
     element: <Root />,
     children: [
       {
-        path: '/',
-        element: <Hero />,
+        index: true,
+        element: (
+          <ProtectedRoute>
+            <Hero />
+          </ProtectedRoute>
+        ),
       },
       {
-        path: '/courses',
-        element: <Courses />,
+        path: 'courses',
+        element: (
+          <ProtectedRoute>
+            <Courses />
+          </ProtectedRoute>
+        ),
       },
       {
-        path: '/profile',
-        element: <Profile />,
+        path: 'profile',
+        element: (
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        ),
       },
     ],
   },
