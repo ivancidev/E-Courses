a8ba495e65f73e5d645e177f9bb3de339d8f289f
Herlan <herlanherbas@gmail.com>
2024-08-30 20:04:23 -0400

    chore: Update ProfileDetails component layout and content

--- src/components/profile/profile-details.tsx
+++ src/components/profile/profile-details.tsx
@@ -2,9 +2,13 @@ import ProfileIcon from '../shared/ui/icons/profile';
 
 export default function ProfileDetails() {
   return (
-    <div>
-      <div>
+    <div className="flex mt-12 gap-16 ml-20">
+      <div className="flex items-center">
         <ProfileIcon />
+      </div>
+      <div>
+        <h1 className="font-bold text-2xl">John Doe</h1>
+        <p>Student of fronted developer</p>
       </div>
     </div>
   );
