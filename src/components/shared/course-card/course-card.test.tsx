8371fb46ea64e05279efc324d0f00ba4a167ee06
Herlan <herlanherbas@gmail.com>
2024-09-04 22:12:55 -0400

    refactor: Add unit test for CourseCard component

---
+++ src/components/shared/course-card/course-card.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { describe, expect, test } from 'vitest';
+import { render } from '@testing-library/react';
+import CourseCard from './course-card';
+import { MemoryRouter } from 'react-router-dom';
+describe('CourseCard', () => {
+  test('display course title', () => {
+    const { getByText } = render(
+      <MemoryRouter>
+        <CourseCard
+          title="React"
+          description="React is a JavaScript library for building user interfaces"
+          price={100}
+          duration="2 hours"
+          imageUrl="https://www.programacion.com.py/wp-content/uploads/2016/11/react-logo-1024x576.png"
+          id="1"
+        />
+        ,
+      </MemoryRouter>,
+    );
+    expect(getByText('React')).toBeInTheDocument();
+  });
+});
